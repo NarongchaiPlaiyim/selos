222dd87aab8e7aacafbf0745567ea452ca839568
Nattapol <nattapol.surarat@the-c-level.com>
2013-09-05 12:27:17 +0700

    Update brms interface

--- src/main/java/com/clevel/selos/controller/WelcomePage.java
+++ src/main/java/com/clevel/selos/controller/WelcomePage.java
@@ -3,6 +3,8 @@ package com.clevel.selos.controller;
 import com.clevel.selos.dao.master.BusinessDescriptionDAO;
 import com.clevel.selos.dao.master.BusinessGroupDAO;
 import com.clevel.selos.integration.*;
+import com.clevel.selos.integration.brms.model.request.PreScreenRequest;
+import com.clevel.selos.integration.brms.model.response.PreScreenResponse;
 import com.clevel.selos.integration.model.CustomerInfo;
 import com.clevel.selos.model.db.master.BusinessDescription;
 import com.clevel.selos.model.db.master.BusinessGroup;
@@ -64,6 +66,8 @@ public class WelcomePage implements Serializable {
 
     @Inject
     RMInterface rm;
+    @Inject
+    BRMSInterface brms;
 
     @Inject
     @Config(name = "selos.system.name")
@@ -75,6 +79,26 @@ public class WelcomePage implements Serializable {
     }
 
     public void testRM() {
+        try {
+            CustomerInfo customerInfo = rm.getCustomerInfo("", RMInterface.CustomerType.INDIVIDUAL, RMInterface.DocumentType.CITIZEN_ID);
+            log.debug("{}",customerInfo);
+        } catch (Exception e) {
+            log.error("",e);
+        }
+        log.debug("system: {}",system);
+    }
+
+    public void testBRRMS() {
+        try {
+            List<PreScreenResponse> preScreenResponseList = brms.checkPreScreenRule(new PreScreenRequest());
+            log.debug("{}",preScreenResponseList);
+        } catch (Exception e) {
+            log.error("",e);
+        }
+        log.debug("system: {}",system);
+    }
+
+    public void testBRMS() {
         try {
             CustomerInfo customerInfo = rm.getCustomerInfo("", RMInterface.CustomerType.INDIVIDUAL, RMInterface.DocumentType.CITIZEN_ID);
             log.debug("{}",customerInfo);
