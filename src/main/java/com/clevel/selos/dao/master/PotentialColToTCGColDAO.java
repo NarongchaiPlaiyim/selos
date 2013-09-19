c6d04e315d174872df8008ec78063c38ea9f9c10
sukanda <sukanda@the-c-level.com>
2013-09-19 11:45:06 +0700

    NCB Detail

--- src/main/java/com/clevel/selos/dao/master/PotentialColToTCGColDAO.java
+++ src/main/java/com/clevel/selos/dao/master/PotentialColToTCGColDAO.java
@@ -1,10 +1,16 @@
 package com.clevel.selos.dao.master;
 
 import com.clevel.selos.dao.GenericDAO;
+import com.clevel.selos.model.db.master.PotentialCollateral;
+import com.clevel.selos.model.db.master.TCGCollateralType;
 import com.clevel.selos.model.db.relation.PotentialColToTCGCol;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 import org.slf4j.Logger;
 
 import javax.inject.Inject;
+import java.util.List;
 
 public class PotentialColToTCGColDAO extends GenericDAO<PotentialColToTCGCol, Integer> {
     @Inject
@@ -12,6 +18,21 @@ public class PotentialColToTCGColDAO extends GenericDAO<PotentialColToTCGCol, In
 
     @Inject
     PotentialColToTCGColDAO(){
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<PotentialColToTCGCol> getListPotentialColToTCGCol(PotentialCollateral potentialCollateral ){
+        log.info("getListPotentialColToTCGCol. (PotentialCollateral: {})", potentialCollateral);
+        Criteria criteria = createCriteria();
+        criteria.add(Restrictions.eq("potentialCollateral", potentialCollateral));
+        criteria.addOrder(Order.asc("potentialCollateral.id"));
+
+        List<PotentialColToTCGCol> list = criteria.list();
+
+        log.info("getList. (result size: {})", list.size());
+
+        return list;
 
     }
 }
