914b993b09a2cdbeafd5504e3f7e15a195b787e8
Nattapol <nattapol.surarat@the-c-level.com>
2013-09-26 20:30:04 +0700

    add NCBBusiness transformer

--- src/main/java/com/clevel/selos/dao/master/SettlementStatusDAO.java
+++ src/main/java/com/clevel/selos/dao/master/SettlementStatusDAO.java
@@ -1,7 +1,10 @@
 package com.clevel.selos.dao.master;
 
 import com.clevel.selos.dao.GenericDAO;
+import com.clevel.selos.model.db.master.CustomerEntity;
 import com.clevel.selos.model.db.master.SettlementStatus;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
 import org.slf4j.Logger;
 
 import javax.inject.Inject;
@@ -12,5 +15,37 @@ public class SettlementStatusDAO extends GenericDAO<SettlementStatus,Integer> {
 
     @Inject
     public SettlementStatusDAO() {
+    }
+
+    public SettlementStatus getIndividualByCode(String code){
+        log.debug("getIndividualByCode. (code: {}",code);
+
+        //set for individual
+        CustomerEntity customerEntity = new CustomerEntity();
+        customerEntity.setId(1);
+
+        Criteria criteria = createCriteria();
+        criteria.add(Restrictions.eq("customerEntity", customerEntity));
+        criteria.add(Restrictions.eq("code", code));
+        SettlementStatus settlementStatus = (SettlementStatus) criteria.uniqueResult();
+
+        log.debug("getIndividualByCode. (code: {})",code);
+        return settlementStatus;
+    }
+
+    public SettlementStatus getJuristicByCode(String code){
+        log.debug("getJuristicByCode. (code: {}",code);
+
+        //set for juristic
+        CustomerEntity customerEntity = new CustomerEntity();
+        customerEntity.setId(1);
+
+        Criteria criteria = createCriteria();
+        criteria.add(Restrictions.eq("customerEntity", customerEntity));
+        criteria.add(Restrictions.eq("code", code));
+        SettlementStatus settlementStatus = (SettlementStatus) criteria.uniqueResult();
+
+        log.debug("getJuristicByCode. (code: {})",code);
+        return settlementStatus;
     }
 }
---
+++ src/main/java/com/clevel/selos/transform/business/BusinessTransform.java
@@ -0,0 +1,5 @@
+package com.clevel.selos.transform.business;
+
+public abstract class BusinessTransform {
+
+}
