8ea8196d7eb7725c6b32ef5017ac2f81780842ca
Rattasak <rattasak.surarat@the-c-level.com>
2013-10-10 18:14:41 +0700

    - Add DAO for CSI

---
+++ src/main/java/com/clevel/selos/dao/master/WarningCodeDAO.java
@@ -0,0 +1,28 @@
+package com.clevel.selos.dao.master;
+
+import com.clevel.selos.dao.GenericDAO;
+import com.clevel.selos.model.db.master.WarningCode;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import org.slf4j.Logger;
+
+import javax.inject.Inject;
+
+public class WarningCodeDAO extends GenericDAO<WarningCode, Integer> {
+    @Inject
+    private Logger log;
+
+    @Inject
+    public WarningCodeDAO(){
+
+    }
+
+    public WarningCode findByCode(String code){
+        log.info("findByCode : {}", code);
+        Criteria criteria = createCriteria();
+        criteria.add(Restrictions.eq("code", code));
+        WarningCode warningCode = (WarningCode)criteria.uniqueResult();
+
+        return warningCode;
+    }
+}
