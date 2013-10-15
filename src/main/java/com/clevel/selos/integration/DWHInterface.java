3e8fb1feb13932a6d0550152035dd3a323112aee
Nattapol <nattapol.surarat@the-c-level.com>
2013-10-15 15:33:21 +0700

    update

---
+++ src/main/java/com/clevel/selos/integration/DWHInterface.java
@@ -0,0 +1,11 @@
+package com.clevel.selos.integration;
+
+import com.clevel.selos.integration.dwh.model.Obligation;
+import com.clevel.selos.integration.rlos.csi.model.CSIInputData;
+import com.clevel.selos.integration.rlos.csi.model.CSIResult;
+
+import java.util.List;
+
+public interface DWHInterface {
+    public List<Obligation> getObligation(String userId, String tmbCusId);
+}
---
+++ src/main/java/com/clevel/selos/integration/brms/model/request/data/RequestedAccountLevel.java
@@ -0,0 +1,45 @@
+package com.clevel.selos.integration.brms.model.request.data;
+
+import com.clevel.selos.model.db.master.ProductProgram;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class RequestedAccountLevel implements Serializable {
+    private ProductProgram productProgram;
+    private String creditFacilityType; //todo: to be enum
+
+    public RequestedAccountLevel() {
+    }
+
+    public RequestedAccountLevel(ProductProgram productProgram, String creditFacilityType) {
+        this.productProgram = productProgram;
+        this.creditFacilityType = creditFacilityType;
+    }
+
+    public ProductProgram getProductProgram() {
+        return productProgram;
+    }
+
+    public void setProductProgram(ProductProgram productProgram) {
+        this.productProgram = productProgram;
+    }
+
+    public String getCreditFacilityType() {
+        return creditFacilityType;
+    }
+
+    public void setCreditFacilityType(String creditFacilityType) {
+        this.creditFacilityType = creditFacilityType;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("productProgram", productProgram)
+                .append("creditFacilityType", creditFacilityType)
+                .toString();
+    }
+}
