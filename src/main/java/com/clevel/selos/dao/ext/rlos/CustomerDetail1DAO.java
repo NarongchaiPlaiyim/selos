28b2c2c9dbb4ed7a592e63e7151f8c85b5ebacf0
Nattapol <nattapol.surarat@the-c-level.com>
2013-10-16 17:02:28 +0700

    update RLOS AppInProcess

---
+++ src/main/java/com/clevel/selos/dao/ext/rlos/CustomerDetail1DAO.java
@@ -0,0 +1,35 @@
+package com.clevel.selos.dao.ext.rlos;
+
+import com.clevel.selos.dao.GenericDAO;
+import com.clevel.selos.model.db.ext.rlos.CustomerDetail1;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Disjunction;
+import org.hibernate.criterion.Restrictions;
+import org.slf4j.Logger;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerDetail1DAO extends GenericDAO<CustomerDetail1,Long> {
+    @Inject
+    private Logger log;
+
+    @Inject
+    public CustomerDetail1DAO() {
+    }
+
+    public List<CustomerDetail1> getListCustomerByCitizenId(List<String> citizenIdList){
+        log.debug("getListCustomerByCitizenId (citizenId : {})",citizenIdList);
+        List<CustomerDetail1> customerDetail1List = new ArrayList<CustomerDetail1>();
+        if(citizenIdList!=null && citizenIdList.size()>0){
+            Criteria criteria = createCriteria();
+            criteria.add(Restrictions.in("citizenId", citizenIdList));
+            customerDetail1List = criteria.list();
+
+            log.debug("getListCustomerByCitizenId , result (customerDetail1List : {})",customerDetail1List);
+        }
+
+        return customerDetail1List;
+    }
+}
--- src/main/java/com/clevel/selos/integration/RLOSInterface.java
+++ src/main/java/com/clevel/selos/integration/RLOSInterface.java
@@ -1,6 +1,7 @@
 package com.clevel.selos.integration;
 
 import com.clevel.selos.filenet.bpm.services.dto.CaseDTO;
+import com.clevel.selos.integration.rlos.appin.model.AppInProcess;
 import com.clevel.selos.integration.rlos.csi.model.CSIData;
 import com.clevel.selos.integration.rlos.csi.model.CSIInputData;
 import com.clevel.selos.integration.rlos.csi.model.CSIResult;
@@ -13,4 +14,5 @@ public interface RLOSInterface {
     enum DocumentType {CITIZEN_ID,PASSPORT,CORPORATE_ID}
 
     public CSIResult getCSIData(String userId, CSIInputData csiInputData); //todo: change return type to CSI View
+    public List<AppInProcess> getAppInProcess(String userId, List<String> citizenIdList);
 }
