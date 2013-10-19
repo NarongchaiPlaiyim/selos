97ee9104f6dd6abe1dd36762a37d328d2849453d
PinthipN <pinthipn@gmail.com>
2013-10-19 19:35:46 +0700

    update existing credit

--- src/main/java/com/clevel/selos/businesscontrol/ExistingCreditControl.java
+++ src/main/java/com/clevel/selos/businesscontrol/ExistingCreditControl.java
@@ -7,6 +7,7 @@ import com.clevel.selos.integration.dwh.obligation.model.Obligation;
 import com.clevel.selos.integration.dwh.obligation.model.ObligationResult;
 import com.clevel.selos.integration.rlos.appin.model.AppInProcess;
 import com.clevel.selos.integration.rlos.appin.model.AppInProcessResult;
+import com.clevel.selos.integration.rlos.appin.model.CustomerDetail;
 import com.clevel.selos.model.ActionResult;
 import com.clevel.selos.model.db.master.Reference;
 import com.clevel.selos.model.view.ActionStatusView;
@@ -47,16 +48,14 @@ public class ExistingCreditControl extends BusinessControl{
 
         ExistingCreditView existingCreditView = getExistingCreditObligation(customerInfoViewList);
 
-        List<ExistingCreditDetailView> existingCreditDetailViewList = new ArrayList<ExistingCreditDetailView>();
+        ExistingCreditView _tmpRLOSAppIn = getRLOSAppInProcess(customerInfoViewList);
+        existingCreditView.setBorrowerAppInRLOSCredit(_tmpRLOSAppIn.getBorrowerAppInRLOSCredit());
+        existingCreditView.setRelatedAppInRLOSCredit(_tmpRLOSAppIn.getRelatedAppInRLOSCredit());
+        existingCreditView.setTotalBorrowerAppInRLOSLimit(_tmpRLOSAppIn.getTotalBorrowerAppInRLOSLimit());
+        existingCreditView.setTotalRelatedAppInRLOSLimit(_tmpRLOSAppIn.getTotalRelatedAppInRLOSLimit());
 
         List<ActionStatusView> actionStatusViewList = new ArrayList<ActionStatusView>();
 
-
-        ActionStatusView actionStatusView = new ActionStatusView();
-        actionStatusView.setStatusCode(ActionResult.SUCCESS);
-        actionStatusView.setStatusDesc("No Account Found");
-
-        actionStatusViewList.add(actionStatusView);
 
         existingCreditView.setStatus(actionStatusViewList);
 
@@ -155,18 +154,41 @@ public class ExistingCreditControl extends BusinessControl{
             AppInProcessResult appInProcessResult = rlosInterface.getAppInProcessData(getCurrentUserID(), personalIDList);
             if(appInProcessResult.getActionResult().equals(ActionResult.SUCCESS)){
 
-                Map<String, ExistingCreditDetailView> borrowerRLOSAppInHashMap = new HashMap<String, ExistingCreditDetailView>();
+                List<ExistingCreditDetailView> borrowerRLOSApp = new ArrayList<ExistingCreditDetailView>();
+                BigDecimal totalBorrowerRLOSApp = BigDecimal.ZERO;
 
-                Map<String, ExistingCreditDetailView> relatedRLOSAppInHashMap = new HashMap<String, ExistingCreditDetailView>();
-
+                List<ExistingCreditDetailView> relatedRLOSApp = new ArrayList<ExistingCreditDetailView>();
+                BigDecimal totalRelatedRLOSApp = BigDecimal.ZERO;
 
                 List<AppInProcess> appInProcessList = appInProcessResult.getAppInProcessList();
                 for(AppInProcess appInProcess : appInProcessList){
+                    ExistingCreditDetailView existingCreditDetailView = existingCreditTransform.getExistingCredit(appInProcess);
+                    List<CustomerDetail> customerDetailList = appInProcess.getCustomerDetailList();
+                    boolean isBorrower = false;
+                    for(CustomerDetail customerDetail : customerDetailList) {
+                        if(_borrowerPersonalID.contains(customerDetail.getCitizenId())){
+                            isBorrower = true;
+                        }
+                    }
+                    if(isBorrower){
+                        borrowerRLOSApp.add(existingCreditDetailView);
+                        totalBorrowerRLOSApp.add(existingCreditDetailView.getLimit());
+                    } else {
+                        relatedRLOSApp.add(existingCreditDetailView);
+                        totalRelatedRLOSApp.add(existingCreditDetailView.getLimit());
+                    }
 
                 }
+                existingCreditView.setBorrowerAppInRLOSCredit(borrowerRLOSApp);
+                existingCreditView.setTotalBorrowerAppInRLOSLimit(totalBorrowerRLOSApp);
+
+                existingCreditView.setRelatedAppInRLOSCredit(relatedRLOSApp);
+                existingCreditView.setTotalRelatedAppInRLOSLimit(totalRelatedRLOSApp);
             }
+
+            //TODO: Update Retrieving Status.
         }
-        return null;
+        return existingCreditView;
     }
 
     /**
