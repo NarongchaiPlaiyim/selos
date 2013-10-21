1f160c8b149f98d08cce47f0c8ca540809140a75
PinthipN <pinthipn@gmail.com>
2013-10-21 18:05:22 +0700

    Merge branch 'pre-develop' of ssh://115.178.62.131/git/git_repos/TMB/SELOS/selos into pre-develop
    Conflicts:
    	src/main/java/com/clevel/selos/transform/BasicInfoAccountTransform.java

--- src/main/java/com/clevel/selos/businesscontrol/InboxControl.java
+++ src/main/java/com/clevel/selos/businesscontrol/InboxControl.java
@@ -53,9 +53,9 @@ public class InboxControl extends BusinessControl {
         List<InboxView> inboxViewList = new ArrayList<InboxView>();
 
         //For WebSphere//
-        //List<CaseDTO> caseDTOList = bpmInterface.getInboxList();
+        List<CaseDTO> caseDTOList = bpmInterface.getInboxList();
 
-        List<CaseDTO> caseDTOList = new ArrayList<CaseDTO>();
+        /*List<CaseDTO> caseDTOList = new ArrayList<CaseDTO>();
 
 
         List<WorkCasePrescreen> workCasePrescreenList = getWorkCasePreScreen();
@@ -75,7 +75,7 @@ public class InboxControl extends BusinessControl {
             caseDTOList.add(caseDTO);
         }
 
-        log.info("CaseDTO : caseDTOList : {}", caseDTOList);
+        log.info("CaseDTO : caseDTOList : {}", caseDTOList);*/
         inboxViewList = inboxBizTransform.transformToView(caseDTOList);
 
         return inboxViewList;
