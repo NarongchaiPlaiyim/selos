dbbba086563062eaca86e00b2a1177fc1f63c39e
Rattasak Surarat <rattasak.surarat@the-c-level.com>
2013-12-16 18:35:26 +0700

    - change way to include css/js

--- src/main/java/com/clevel/selos/businesscontrol/InboxControl.java
+++ src/main/java/com/clevel/selos/businesscontrol/InboxControl.java
@@ -64,9 +64,9 @@ public class InboxControl extends BusinessControl {
         List<InboxView> inboxViewList = new ArrayList<InboxView>();
 
         //For WebSphere//
-        //List<CaseDTO> caseDTOList = bpmInterface.getInboxList();
+        List<CaseDTO> caseDTOList = bpmInterface.getInboxList();
 
-        List<CaseDTO> caseDTOList = new ArrayList<CaseDTO>();
+        /*List<CaseDTO> caseDTOList = new ArrayList<CaseDTO>();
 
 
         List<WorkCasePrescreen> workCasePrescreenList = getWorkCasePreScreen();
@@ -101,7 +101,7 @@ public class InboxControl extends BusinessControl {
             caseDTO.setCaseData(caseData);
 
             caseDTOList.add(caseDTO);
-        }
+        }*/
 
         log.info("CaseDTO : caseDTOList : {}", caseDTOList);
         inboxViewList = inboxBizTransform.transformToView(caseDTOList);
