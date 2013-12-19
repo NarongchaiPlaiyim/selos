58e24edff5630c1b0b9c2f964b03072c9023b4f4
Rattasak Surarat <rattasak.surarat@the-c-level.com>
2013-12-19 14:56:36 +0700

    -add relation for subcollateral

--- src/main/java/com/clevel/selos/model/db/working/NewCollateralSubDetail.java
+++ src/main/java/com/clevel/selos/model/db/working/NewCollateralSubDetail.java
@@ -65,20 +65,14 @@ public class NewCollateralSubDetail implements Serializable {
     @JoinColumn(name = "new_collateral_head_detail_id")
     private NewCollateralHeadDetail newCollateralHeadDetail;
 
-    @OneToMany
-    @JoinColumn(name = "mortgage_type_id")
-    private List<MortgageType> mortgageList;
+    @OneToMany(mappedBy = "newCollateralSubDetail", cascade = CascadeType.ALL)
+    private List<NewCollateralSubMortgage> newCollateralSubMortgageList;
 
     @OneToMany(mappedBy = "newCollateralSubDetail", cascade = CascadeType.ALL)
     private List<NewCollateralSubCustomer> newCollateralSubCustomerList;
 
-    /*@OneToMany(mappedBy = "newCollateralSubDetail", cascade = CascadeType.ALL)
-    private List<NewCollateralSubDetail> relatedWithList;
-
-    @ManyToOne
-    @JoinColumn(name = "new_collateral_sub_detail_id")
-    private NewCollateralSubDetail newCollateralSubDetail;*/
-
+    @OneToMany(mappedBy = "newCollateralSubDetail", cascade = CascadeType.ALL)
+    private List<NewCollateralSubRelate> newCollateralSubRelateList;
 
     public long getId() {
         return id;
@@ -192,30 +186,27 @@ public class NewCollateralSubDetail implements Serializable {
         this.newCollateralHeadDetail = newCollateralHeadDetail;
     }
 
-    public List<MortgageType> getMortgageList() {
-        return mortgageList;
+    public List<NewCollateralSubMortgage> getNewCollateralSubMortgageList() {
+        return newCollateralSubMortgageList;
     }
 
-    public void setMortgageList(List<MortgageType> mortgageList) {
-        this.mortgageList = mortgageList;
+    public void setNewCollateralSubMortgageList(List<NewCollateralSubMortgage> newCollateralSubMortgageList) {
+        this.newCollateralSubMortgageList = newCollateralSubMortgageList;
     }
 
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
-                .append("id", id)
-                .append("subCollTypeCaption", subCollTypeCaption)
-                .append("address", address)
-                .append("landOffice", landOffice)
-                .append("titleDeed", titleDeed)
-                .append("collateralOwner", collateralOwner)
-                .append("collateralOwnerAAD", collateralOwnerAAD)
-                .append("appraisalValue", appraisalValue)
-                .append("mortgageValue", mortgageValue)
-                .append("createDate", createDate)
-                .append("modifyDate", modifyDate)
-                .append("createBy", createBy)
-                .append("modifyBy", modifyBy)
-                .toString();
+    public List<NewCollateralSubCustomer> getNewCollateralSubCustomerList() {
+        return newCollateralSubCustomerList;
+    }
+
+    public void setNewCollateralSubCustomerList(List<NewCollateralSubCustomer> newCollateralSubCustomerList) {
+        this.newCollateralSubCustomerList = newCollateralSubCustomerList;
+    }
+
+    public List<NewCollateralSubRelate> getNewCollateralSubRelateList() {
+        return newCollateralSubRelateList;
+    }
+
+    public void setNewCollateralSubRelateList(List<NewCollateralSubRelate> newCollateralSubRelateList) {
+        this.newCollateralSubRelateList = newCollateralSubRelateList;
     }
 }
