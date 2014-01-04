0cdf44296976cfd2b910597bee396153ffdb0b37
Chai <narongchai@the-c-level.com>
2014-01-04 17:30:28 +0700

    Transform

--- src/main/java/com/clevel/selos/transform/AccountInfoDetailTransform.java
+++ src/main/java/com/clevel/selos/transform/AccountInfoDetailTransform.java
@@ -7,8 +7,16 @@ import com.clevel.selos.model.view.AccountInfoDetailView;
 import javax.inject.Inject;
 
 public class AccountInfoDetailTransform extends Transform {
+    @Inject
+    AccountInfoBranchTransform branchTransform;
+    @Inject
+    AccountInfoAccountTypeTransform accountTypeTransform;
+    @Inject
+    AccountInfoProductTypeTransform productTypeTransform;
+
     private AccountInfoDetail accountInfoDetail;
 
+    private long id;
     @Inject
     public AccountInfoDetailTransform() {
 
@@ -20,12 +28,37 @@ public class AccountInfoDetailTransform extends Transform {
         accountInfoDetail.setRequestAccountType(accountInfoDetailView.getReqAccountType());
         accountInfoDetail.setAccountNumber(accountInfoDetailView.getAccountNumber());
 
-        //Object from master table
-        accountInfoDetail.setBankBranch(null);
-        accountInfoDetail.setAccountType(null);
-        accountInfoDetail.setProductType(null);
+        //Branch
+        id = accountInfoDetailView.getBranchView().getId();
+        if(id != 0){
+            accountInfoDetail.setBankBranch(branchTransform.transformToModel((int)id));
+        } else {
+            accountInfoDetail.setBankBranch(null);
+        }
+
+        //Account Type
+        id = accountInfoDetailView.getAccountTypeView().getId();
+        if(id != 0){
+            accountInfoDetail.setAccountType(accountTypeTransform.transformToModel((int)id));
+        } else {
+            accountInfoDetail.setAccountType(null);
+        }
+
+        //Product Type
+        id = accountInfoDetailView.getProductTypeView().getId();
+        if(id != 0){
+            accountInfoDetail.setProductType(productTypeTransform.transformToModel((int)id));
+        } else {
+            accountInfoDetail.setProductType(null);
+        }
+
+        //Account Name
         accountInfoDetail.setAccountNameList(null);
+
+        //Purpose
         accountInfoDetail.setPurposeList(null);
+
+        //Credit Type
         accountInfoDetail.setCreditTypeList(null);
 
         accountInfoDetail.setTerm(accountInfoDetailView.getTerm());
