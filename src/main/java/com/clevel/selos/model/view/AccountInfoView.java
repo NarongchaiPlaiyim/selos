c9295de581aa9fb15f77627af3aa9efbe22b0b56
Chai <narongchai@the-c-level.com>
2014-01-08 10:31:46 +0700

    Account Information

--- src/main/java/com/clevel/selos/model/view/AccountInfoView.java
+++ src/main/java/com/clevel/selos/model/view/AccountInfoView.java
@@ -1,10 +1,12 @@
 package com.clevel.selos.model.view;
 
+import com.clevel.selos.model.db.master.User;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class AccountInfoView implements Serializable {
@@ -12,6 +14,10 @@ public class AccountInfoView implements Serializable {
     private int approvedType;
     private List<AccountInfoDetailView> accountInfoDetailViewList;
     private AccountInfoDetailView accountInfoDetailViewSelected;
+    private Date createDate;
+    private Date modifyDate;
+    private User createBy;
+    private User modifyBy;
 
     public AccountInfoView(){
         init();
@@ -58,13 +64,49 @@ public class AccountInfoView implements Serializable {
         this.accountInfoDetailViewSelected = accountInfoDetailViewSelected;
     }
 
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    public User getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(User createBy) {
+        this.createBy = createBy;
+    }
+
+    public User getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(User modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
     @Override
     public String toString() {
         return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
                 .append("id", id)
                 .append("approvedType", approvedType)
                 .append("accountInfoDetailViewList", accountInfoDetailViewList)
-                .append("AccountInfoDetailViewSelected", accountInfoDetailViewSelected)
+                .append("accountInfoDetailViewSelected", accountInfoDetailViewSelected)
+                .append("createDate", createDate)
+                .append("modifyDate", modifyDate)
+                .append("createBy", createBy)
+                .append("modifyBy", modifyBy)
                 .toString();
     }
 }
