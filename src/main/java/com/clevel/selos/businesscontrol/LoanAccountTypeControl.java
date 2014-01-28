8ef4d8eb196685762fc552540c5fc183cfcab753
PinthipN <pinthipn@gmail.com>
2014-01-28 16:54:54 +0700

    Merge branch 'pre-develop' of ssh://115.178.62.131/git/tmb/selos/selos into pre-develop
    Conflicts:
    	src/main/java/com/clevel/selos/businesscontrol/AppraisalResultControl.java
    	src/main/java/com/clevel/selos/businesscontrol/LoanAccountTypeControl.java
    	src/main/java/com/clevel/selos/model/db/working/NewCollateralCredit.java

--- src/main/java/com/clevel/selos/businesscontrol/LoanAccountTypeControl.java
+++ src/main/java/com/clevel/selos/businesscontrol/LoanAccountTypeControl.java
@@ -2,10 +2,8 @@ package com.clevel.selos.businesscontrol;
 
 import com.clevel.selos.dao.master.AccountTypeDAO;
 import com.clevel.selos.dao.working.BasicInfoDAO;
-import com.clevel.selos.dao.working.WorkCaseDAO;
 import com.clevel.selos.model.db.master.AccountType;
 import com.clevel.selos.model.db.working.BasicInfo;
-import com.clevel.selos.model.db.working.WorkCase;
 import com.clevel.selos.model.view.LoanAccountTypeView;
 import com.clevel.selos.transform.LoanAccountTypeTransform;
 
@@ -31,14 +29,11 @@ public class LoanAccountTypeControl extends BusinessControl {
 
     }
 
-	@Inject
-    WorkCaseDAO workCaseDAO;
-
     public List<LoanAccountTypeView> getListLoanTypeByWorkcase(long workCaseId) {
-        WorkCase workCase = workCaseDAO.findById(workCaseId);
+        BasicInfo basicInfo = basicInfoDAO.findByWorkCaseId(workCaseId);
         List<AccountType> loanAccountTypes = new ArrayList<AccountType>();
-        if(workCase.getBorrowerType().getId() != 0){
-           loanAccountTypes = loanAccountTypeDAO.getListLoanTypeByCusEntity(workCase.getBorrowerType().getId());
+        if(basicInfo.getBorrowerType().getId() != 0){
+           loanAccountTypes = loanAccountTypeDAO.getListLoanTypeByCusEntity(basicInfo.getBorrowerType().getId());
         }
         List<LoanAccountTypeView> loanTypeViews = loanAccountTypeTransform.getLoanAccountTypeViews(loanAccountTypes);
         return loanTypeViews;
