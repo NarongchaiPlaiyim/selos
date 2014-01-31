aeac46794f0c85e4d7cc39bf3c01a9faf8326375
Apiruk <apriuk@the-c-level.com>
2014-01-31 16:13:44 +0700

    Fixed NCB Month Thai

--- src/main/java/com/clevel/selos/model/Month.java
+++ src/main/java/com/clevel/selos/model/Month.java
@@ -3,32 +3,19 @@ package com.clevel.selos.model;
 public enum Month {
 
     //    January(1), February(2), March(3), April(4), May(5), June(6), July(7), August(8), September(9), October(10), November(11), December(12);
-    January(1,"มกราคม"),
-    February(2,"กุมภาพันธ์"),
-    March(3,"มีนาคม"),
-    April(4,"เมษายน"),
-    May(5,"พฤษภาคม"),
-    June(6,"มิถุนายน"),
-    July(7,"กรกฎาคม"),
-    August(8,"สิงหาคม"),
-    September(9,"กันยายน"),
-    October(10,"ตุลาคม"),
-    November(11,"พฤศจิกายน"),
-    December(12,"ธันวาคม");
-
+    มกราคม(1), กุมภาพันธ์(2), มีนาคม(3), เมษายน(4), พฤษภาคม(5), มิถุนายน(6), กรกฎาคม(7), สิงหาคม(8), กันยายน(9), ตุลาคม(10), พฤศจิกายน(11), ธันวาคม(12);
     int value;
-    String nameTH;
 
-    private Month(int value, String nameTH) {
+
+    Month(int value) {
         this.value = value;
-        this.nameTH = nameTH;
+    }
+
+    public int getValue() {
+        return value;
     }
 
     public int value() {
         return this.value;
-    }
-
-    public String nameTH() {
-        return this.nameTH;
     }
 }
