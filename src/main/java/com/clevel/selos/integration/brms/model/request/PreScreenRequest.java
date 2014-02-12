090c203bba15865be00264389a78a646f26c5b8d
PinthipN <pinthipn@gmail.com>
2014-02-12 23:12:59 +0700

    Remove unnecessary files

--- src/main/java/com/clevel/selos/integration/brms/model/request/PreScreenRequest.java
+++ src/main/java/com/clevel/selos/integration/brms/model/request/PreScreenRequest.java
@@ -8,52 +8,7 @@ import java.io.Serializable;
 import java.util.List;
 
 public class PreScreenRequest implements Serializable {
-//    private ApplicationLevel applicationLevel;
-//    private List<BorrowerLevel> customerLevelList;
-//    private String bizDescription;
-//    private TmbAccountLevel tmbAccountLevel;
-//
-//    public PreScreenRequest() {
-//    }
-//
-//    public PreScreenRequest(ApplicationLevel applicationLevel, List<BorrowerLevel> customerLevelList, String bizDescription, TmbAccountLevel tmbAccountLevel) {
-//        this.applicationLevel = applicationLevel;
-//        this.customerLevelList = customerLevelList;
-//        this.bizDescription = bizDescription;
-//        this.tmbAccountLevel = tmbAccountLevel;
-//    }
-//
-//    public ApplicationLevel getApplicationLevel() {
-//        return applicationLevel;
-//    }
-//
-//    public void setApplicationLevel(ApplicationLevel applicationLevel) {
-//        this.applicationLevel = applicationLevel;
-//    }
-//
-//    public List<BorrowerLevel> getCustomerLevelList() {
-//        return customerLevelList;
-//    }
-//
-//    public void setCustomerLevelList(List<BorrowerLevel> customerLevelList) {
-//        this.customerLevelList = customerLevelList;
-//    }
-//
-//    public String getBizDescription() {
-//        return bizDescription;
-//    }
-//
-//    public void setBizDescription(String bizDescription) {
-//        this.bizDescription = bizDescription;
-//    }
-//
-//    public TmbAccountLevel getTmbAccountLevel() {
-//        return tmbAccountLevel;
-//    }
-//
-//    public void setTmbAccountLevel(TmbAccountLevel tmbAccountLevel) {
-//        this.tmbAccountLevel = tmbAccountLevel;
-//    }
+
     private ApplicationTypeLevel applicationType;
 
     public PreScreenRequest() {
