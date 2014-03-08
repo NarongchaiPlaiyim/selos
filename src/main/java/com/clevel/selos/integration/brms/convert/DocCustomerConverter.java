0d85af26a9730e9478ae56b2ac97bc6a4a578e73
PinthipN <pinthipn@gmail.com>
2014-03-08 13:59:48 +0700

    Fix Circulate inject issue.

---
+++ src/main/java/com/clevel/selos/integration/brms/convert/DocCustomerConverter.java
@@ -0,0 +1,21 @@
+package com.clevel.selos.integration.brms.convert;
+
+import com.clevel.selos.integration.SELOS;
+import org.slf4j.Logger;
+
+import javax.inject.Inject;
+
+public class DocCustomerConverter extends Converter{
+
+    @Inject
+    @SELOS
+    Logger logger;
+
+    @Inject
+    public DocCustomerConverter(){
+
+    }
+
+
+
+}
