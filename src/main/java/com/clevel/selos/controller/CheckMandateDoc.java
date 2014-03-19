3c727abeef230b835c4e42adec5f8ee2850479dd
Chai <narongchai@the-c-level.com>
2014-03-19 17:11:12 +0700

    maxlength="20"

--- src/main/java/com/clevel/selos/controller/CheckMandateDoc.java
+++ src/main/java/com/clevel/selos/controller/CheckMandateDoc.java
@@ -1,5 +1,6 @@
 package com.clevel.selos.controller;
 
+import com.clevel.selos.businesscontrol.CheckMandateDocControl;
 import com.clevel.selos.exception.ECMInterfaceException;
 import com.clevel.selos.integration.ECM;
 import com.clevel.selos.integration.ECMInterface;
@@ -8,6 +9,7 @@ import com.clevel.selos.integration.ecm.db.ECMDetail;
 import com.clevel.selos.integration.ecm.model.ECMDataResult;
 import com.clevel.selos.integration.filenet.ce.connection.CESessionToken;
 import com.clevel.selos.model.ActionResult;
+import com.clevel.selos.model.view.CheckMandateDocView;
 import com.clevel.selos.model.view.CheckMandatoryDocView;
 import com.clevel.selos.model.view.CheckOptionalDocView;
 import com.clevel.selos.model.view.CheckOtherDocView;
@@ -23,7 +25,9 @@ import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ViewScoped
 @ManagedBean(name = "checkMandateDoc")
@@ -32,30 +36,8 @@ public class CheckMandateDoc implements Serializable {
     @ECM
     private Logger log;
     @Inject
-    @Config(name = "interface.workplace.address")
-    private String address;
-    @Inject
-    @Config(name = "interface.mandate.doc.objectStore")
-    private String objectStore;
-    @Inject
-    @Config(name = "interface.workplace.username")
-    private String username;
-    @Inject
-    @Config(name = "interface.workplace.password")
-    private String password;
-    @Inject
-    @Config(name = "system.encryption.enable")
-    private String encryptionEnable;
-    @Inject
-    private EncryptionService encryptionService;
-    @Inject
-    private CESessionToken CESessionToken;
-    private String passwordEncrypt;
-
-    private List<CheckMandatoryDocView> mandatoryDocumentsList;
-    private List<CheckOptionalDocView> optionalDocumentsList;
-    private List<CheckOtherDocView> otherDocumentsList;
-
+    private CheckMandateDocControl checkMandateDocControl;
+    private CheckMandateDocView checkMandateDocView;
     private int rowIndex;
     private String messageHeader;
     private String message;
@@ -70,85 +52,66 @@ public class CheckMandateDoc implements Serializable {
     }
 
     private void init(){
-        mandatoryDocumentsList = new ArrayList<CheckMandatoryDocView>();
-        CheckMandatoryDocView checkMandatoryDocView = new CheckMandatoryDocView();
-        checkMandatoryDocView.setDocumentType("6.8.11 Document Ttype");
-        checkMandatoryDocView.setBRMSDocument("Citizen ID");
-        checkMandatoryDocView.setOwners("Mr. Narongchai");
-        checkMandatoryDocView.setFileName("File_2013107_1501");
-        checkMandatoryDocView.setDisabledCompleteFlag(true);
-        checkMandatoryDocView.setComplete(1);
-        checkMandatoryDocView.setIndistinct(true);
-        checkMandatoryDocView.setRemark("test");
-        mandatoryDocumentsList.add(checkMandatoryDocView);
-
-        checkMandatoryDocView = new CheckMandatoryDocView();
-        checkMandatoryDocView.setDocumentType("6.9.3 Statement");
-        checkMandatoryDocView.setBRMSDocument("Citizen ID");
-        checkMandatoryDocView.setOwners("Mr. Narongchai2");
-        checkMandatoryDocView.setFileName("File_2013107_1502");
-        checkMandatoryDocView.setComplete(3);
-        checkMandatoryDocView.setIndistinct(true);
-        checkMandatoryDocView.setExpire(true);
-        checkMandatoryDocView.setRemark("test");
-        mandatoryDocumentsList.add(checkMandatoryDocView);
-
-        optionalDocumentsList = new ArrayList<CheckOptionalDocView>();
-        CheckOptionalDocView checkOptionalDocView = new CheckOptionalDocView();
-        checkOptionalDocView.setDocumentType("6.11.2 Statement");
-        checkOptionalDocView.setOwners("Application");
-        checkOptionalDocView.setFileName("File_2013107_1502");
-        checkOptionalDocView.setComplete(2);
-        checkOptionalDocView.setIndistinct(true);
-        checkOptionalDocView.setExpire(true);
-        checkOptionalDocView.setRemark("test");
-        optionalDocumentsList.add(checkOptionalDocView);
-
-        checkOptionalDocView = new CheckOptionalDocView();
-        checkOptionalDocView.setDocumentType("X.X.XXXX StatementXXXXX");
-        checkOptionalDocView.setBRMSDocument("XXXXXXXXXXX");
-        checkOptionalDocView.setOwners("Application");
-        checkOptionalDocView.setFileName("File_2013107_1502");
-        checkOptionalDocView.setComplete(2);
-        checkOptionalDocView.setIndistinct(true);
-        checkOptionalDocView.setExpire(true);
-        checkOptionalDocView.setRemark("test");
-        optionalDocumentsList.add(checkOptionalDocView);
+        checkMandateDocView = new CheckMandateDocView();
     }
 
     @PostConstruct
     public void onCreation() {
+
         log.info("-- onCreation.");
-        if (Util.isTrue(encryptionEnable)) {
-            passwordEncrypt = encryptionService.decrypt(Base64.decodeBase64(password));
-        } else {
-            passwordEncrypt = password;
-        }
-        log.debug("-- User = {}", username);
-        log.debug("-- Password = {}", password);
-        log.debug("-- Password[Decrypt] = {}", passwordEncrypt);
-        otherDocumentsList = new ArrayList<CheckOtherDocView>();
+//        if (Util.isTrue(encryptionEnable)) {
+//            passwordEncrypt = encryptionService.decrypt(Base64.decodeBase64(password));
+//        } else {
+//            passwordEncrypt = password;
+//        }
+//        log.debug("-- User = {}", username);
+//        log.debug("-- Password = {}", password);
+//        log.debug("-- Password[Decrypt] = {}", passwordEncrypt);
+//        otherDocumentsList = new ArrayList<CheckOtherDocView>();
         String result = null;
+        String caNumber = "11111111111111111111";
+
         try{
-            ECMDataResult ecmDataResult = ecmInterface.getECMDataResult("11111111111111111111");
+            //Calling ECM
+            ECMDataResult ecmDataResult = ecmInterface.getECMDataResult(caNumber);
+
+
+
             if(!Util.isNull(ecmDataResult) && ActionResult.SUCCESS.equals(ecmDataResult.getActionResult())){
                 List<ECMDetail> ecmDetailList = Util.safetyList(ecmDataResult.getEcmDetailList());
                 CheckOtherDocView checkOtherDocView = null;
-                String userToken = CESessionToken.getTokenFromSession(username, passwordEncrypt);
-                log.debug("-- User Token {}", userToken);
+//                String userToken = CESessionToken.getTokenFromSession(username, passwordEncrypt);
+//                log.debug("-- User Token {}", userToken);
+
+                Map<String,List<ECMDetail>> ecmMap = new HashMap<String, List<ECMDetail>>();
+
+
                 for (ECMDetail ecmDetail : ecmDetailList) {
+
+                    if(ecmMap.containsKey(ecmDetail.getEcmDocId())){
+                        List<ECMDetail> ecmListTmp = ecmMap.get(ecmDetail.getEcmDocId());
+                        ecmListTmp.add(ecmDetail);
+                        ecmMap.put(ecmDetail.getEcmDocId(),ecmListTmp);
+                    } else {
+                        List<ECMDetail> ecmListTmp = new ArrayList<ECMDetail>();
+                        ecmListTmp.add(ecmDetail);
+                        ecmMap.put(ecmDetail.getEcmDocId(),ecmListTmp);
+                    }
+
+
+
                     checkOtherDocView = new CheckOtherDocView();
                     checkOtherDocView.setDocumentType(ecmDetail.getTypeNameTH());
                     checkOtherDocView.setOwners(ecmDetail.getTypeCode());
                     checkOtherDocView.setFileName(ecmDetail.getOrgFileName());
-                    result = getURLByFNId(ecmDetail.getFnDocId(), userToken);
+//                    result = getURLByFNId(ecmDetail.getFnDocId(), userToken);
                     checkOtherDocView.setLink(result);
                     checkOtherDocView.setComplete(2);
                     checkOtherDocView.setIndistinct(true);
                     checkOtherDocView.setExpire(true);
                     checkOtherDocView.setRemark("test");
                     log.debug("-- Link[{}]", result);
-                    otherDocumentsList.add(checkOtherDocView);
+//                    otherDocumentsList.add(checkOtherDocView);
                 }
                 result = ecmDetailList.toString();
             } else {
@@ -171,34 +134,12 @@ public class CheckMandateDoc implements Serializable {
 
     }
 
-
-    private String getURLByFNId(final String FNId, final String token){
-        return address+"/getContent?objectStoreName="+objectStore+"&id="+FNId+"&objectType=document&ut=" + token;
+    public CheckMandateDocView getCheckMandateDocView() {
+        return checkMandateDocView;
     }
 
-
-    public List<CheckMandatoryDocView> getMandatoryDocumentsList() {
-        return mandatoryDocumentsList;
-    }
-
-    public void setMandatoryDocumentsList(List<CheckMandatoryDocView> mandatoryDocumentsList) {
-        this.mandatoryDocumentsList = mandatoryDocumentsList;
-    }
-
-    public List<CheckOptionalDocView> getOptionalDocumentsList() {
-        return optionalDocumentsList;
-    }
-
-    public void setOptionalDocumentsList(List<CheckOptionalDocView> optionalDocumentsList) {
-        this.optionalDocumentsList = optionalDocumentsList;
-    }
-
-    public List<CheckOtherDocView> getOtherDocumentsList() {
-        return otherDocumentsList;
-    }
-
-    public void setOtherDocumentsList(List<CheckOtherDocView> otherDocumentsList) {
-        this.otherDocumentsList = otherDocumentsList;
+    public void setCheckMandateDocView(CheckMandateDocView checkMandateDocView) {
+        this.checkMandateDocView = checkMandateDocView;
     }
 
     public int getRowIndex() {
