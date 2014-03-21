43612c58d328d7a3385fa27431b3d137ee1df89b
Apiruk <apriuk@the-c-level.com>
2014-03-21 17:15:34 +0700

    Add extends Transform

--- src/main/java/com/clevel/selos/transform/AppraisalDetailTransform.java
+++ src/main/java/com/clevel/selos/transform/AppraisalDetailTransform.java
@@ -21,7 +21,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AppraisalDetailTransform {
+public class AppraisalDetailTransform extends Transform {
     @Inject
     @SELOS
     Logger log;
