1114985f188fd48e53459b2c98b498fa8fb070c3
Prashanth Reddy B <prashanthreddy.bollepally@the-c-level.com>
2014-04-25 16:12:27 +0700

    completed cases

--- src/main/java/com/clevel/selos/controller/PESearch.java
+++ src/main/java/com/clevel/selos/controller/PESearch.java
@@ -664,7 +664,29 @@ public class PESearch implements Serializable
                 wrkCaseAppraisalId = workCaseAppraisal.getId();
                 session.setAttribute("workCaseAppraisalId", wrkCaseAppraisalId);
             }
-        } else {        //For Case in Stage FullApplication
+        }
+        else if(stepId == 0) //for completed cases
+        {
+
+            WorkCase workCase1 = workCaseDAO.findByAppNumber(appNumber);
+            if(workCase!= null)
+            {
+                wrkCaseId = workCase1.getId();
+                requestAppraisalFlag = workCase1.getRequestAppraisal();
+                session.setAttribute("workCaseId", wrkCaseId);
+                session.setAttribute("requestAppraisal", requestAppraisalFlag);
+            }
+            else
+            {
+                WorkCasePrescreen workCasePrescreen = workCasePrescreenDAO.findByAppNumber(appNumber);
+                wrkCasePreScreenId = workCasePrescreen.getId();
+                requestAppraisalFlag = workCasePrescreen.getRequestAppraisal();
+                session.setAttribute("workCasePreScreenId", wrkCasePreScreenId);
+                session.setAttribute("requestAppraisal", requestAppraisalFlag);
+            }
+
+        }
+        else {        //For Case in Stage FullApplication
             WorkCase workCase1 = workCaseDAO.findByAppNumber(appNumber);
             if(workCase1 != null){
                 wrkCaseId = workCase1.getId();
