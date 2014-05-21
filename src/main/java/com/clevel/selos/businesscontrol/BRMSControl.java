a97374cbe320f4f9bc6f1a8d9eeaedc5ca5f2405
Nattapol <nattapol.surarat@the-c-level.com>
2014-05-21 11:35:47 +0700

    update brms fullapp

--- src/main/java/com/clevel/selos/businesscontrol/BRMSControl.java
+++ src/main/java/com/clevel/selos/businesscontrol/BRMSControl.java
@@ -432,12 +432,17 @@ public class BRMSControl extends BusinessControl {
         if( _proposeType.equals(ProposeType.P)){
             if(newCreditFacility.getLoanRequestType() != null)
                 applicationInfo.setLoanRequestType(newCreditFacility.getLoanRequestType().getBrmsCode());
+            applicationInfo.setFinalGroupExposure(newCreditFacility.getTotalExposure());
         }
         else if(_proposeType.equals(ProposeType.A)){
             if(newCreditFacility.getLoanRequestType() != null)
                 applicationInfo.setLoanRequestType(newCreditFacility.getLoanRequestType().getBrmsCode());
-            if(decision != null)
+            if(decision != null){
                 applicationInfo.setFinalGroupExposure(decision.getTotalApproveExposure());
+            } else {
+                applicationInfo.setFinalGroupExposure(BigDecimal.ZERO);
+            }
+
         }
 
         BigDecimal totalApprovedCredit = BigDecimal.ZERO;
