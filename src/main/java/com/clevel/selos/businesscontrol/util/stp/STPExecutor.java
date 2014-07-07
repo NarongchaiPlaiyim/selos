2524068bb8022286f257b227bebf9d34615cdcfd
pakorn <pakorn@the-c-level.com>
2014-07-07 15:15:31 +0700

    ISA Report (LogonOver90,UserProfile and Violation)

--- src/main/java/com/clevel/selos/businesscontrol/util/stp/STPExecutor.java
+++ src/main/java/com/clevel/selos/businesscontrol/util/stp/STPExecutor.java
@@ -189,7 +189,7 @@ public class STPExecutor implements Serializable {
             public void execute(Connection connection) throws SQLException {
                 try{
                     CallableStatement callStmt=connection.prepareCall("call SLOS.logonover90 ( ? )");
-                    callStmt.registerOutParameter("cursor_out",OracleTypes.CURSOR);
+                    callStmt.registerOutParameter(1,OracleTypes.CURSOR);
                     callStmt.execute();
                     rs[0] = (ResultSet) callStmt.getObject("cursor_out");
                 } catch (Exception e){
@@ -211,7 +211,7 @@ public class STPExecutor implements Serializable {
                     for (String key : map.keySet()){
                         callStmt.setObject(key, map.get(key).toString());
                     }
-                    callStmt.registerOutParameter("cursor_out",OracleTypes.CURSOR);
+                    callStmt.registerOutParameter(3,OracleTypes.CURSOR);
                     callStmt.executeUpdate();
                     rs[0] = (ResultSet) callStmt.getObject("cursor_out");
                 } catch (Exception e){
@@ -223,7 +223,7 @@ public class STPExecutor implements Serializable {
         return rs[0];
     }
 
-    public ResultSet getuserProfile(final Map<String, Object> map){
+    public ResultSet getuserProfile(){
         log.debug("on getViolation.");
         final ResultSet[] rs = {null};
         ((Session) em.getDelegate()).doWork(new Work() {
@@ -231,10 +231,7 @@ public class STPExecutor implements Serializable {
             public void execute(Connection connection) throws SQLException {
                 try{
                     CallableStatement callStmt = connection.prepareCall("call SLOS.userprofile (?)");
-                    for (String key : map.keySet()){
-                        callStmt.setObject(key, map.get(key).toString());
-                    }
-                    callStmt.registerOutParameter("cursor_out",OracleTypes.CURSOR);
+                    callStmt.registerOutParameter(1,OracleTypes.CURSOR);
                     callStmt.executeUpdate();
                     rs[0] = (ResultSet) callStmt.getObject("cursor_out");
                 } catch (Exception e){
