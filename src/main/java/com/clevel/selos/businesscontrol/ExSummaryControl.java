e3e0cc8d53ef66d0dbe52991174aaee137767418
Nattapol <nattapol.surarat@the-c-level.com>
2014-07-09 22:30:08 +0700

    update exsum

--- src/main/java/com/clevel/selos/businesscontrol/ExSummaryControl.java
+++ src/main/java/com/clevel/selos/businesscontrol/ExSummaryControl.java
@@ -902,13 +902,12 @@ public class ExSummaryControl extends BusinessControl {
         NewCreditFacility newCreditFacility = newCreditFacilityDAO.findByWorkCaseId(workCaseId);
         Decision decision = decisionDAO.findByWorkCaseId(workCaseId);
         ExistingCreditFacility existingCreditFacility = existingCreditFacilityDAO.findByWorkCaseId(workCaseId);
-        BigDecimal groupExposureBDM = BigDecimal.ZERO;
-        BigDecimal groupExposureUW = BigDecimal.ZERO;
-        if(!Util.isNull(newCreditFacility) && !Util.isZero(newCreditFacility.getId())
-            && !Util.isNull(existingCreditFacility) && !Util.isZero(existingCreditFacility.getId())){
-            groupExposureBDM = Util.add(existingCreditFacility.getTotalGroupExposure(), newCreditFacility.getTotalPropose());
+        BigDecimal groupExposureBDM = null;
+        BigDecimal groupExposureUW = null;
+        if(!Util.isNull(newCreditFacility) && !Util.isZero(newCreditFacility.getId())){
+            groupExposureBDM = Util.add(newCreditFacility.getTotalExposure(), newCreditFacility.getTotalPropose());
             if(!Util.isNull(decision) && !Util.isZero(decision.getId())){
-                groupExposureUW = Util.add(existingCreditFacility.getTotalGroupExposure(), decision.getTotalApproveCredit());
+                groupExposureUW = Util.add(newCreditFacility.getTotalExposure(), decision.getTotalApproveCredit());
             }
         }
 
@@ -920,12 +919,15 @@ public class ExSummaryControl extends BusinessControl {
             exSummary.setWorkCase(workCase);
         }
 
-        User user = getCurrentUser();
+        exSummary.setGroupExposureUW(groupExposureUW);
+        exSummary.setGroupExposureBDM(groupExposureBDM);
+
+        /*User user = getCurrentUser();
         if(user.getRole().getId() == RoleValue.UW.id()){
             exSummary.setGroupExposureUW(groupExposureUW);
         } else {
             exSummary.setGroupExposureBDM(groupExposureBDM);
-        }
+        }*/
 
         exSummaryDAO.persist(exSummary);
     }
