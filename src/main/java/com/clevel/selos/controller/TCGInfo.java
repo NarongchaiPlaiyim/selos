e650b849e91880c1eb9311b5ab88df955bd9e09f
Apiruk <apriuk@the-c-level.com>
2014-09-19 00:43:29 +0700

    Add Auto Cal for TCG & Basic on ExSummary Screen

--- src/main/java/com/clevel/selos/controller/TCGInfo.java
+++ src/main/java/com/clevel/selos/controller/TCGInfo.java
@@ -1,5 +1,6 @@
 package com.clevel.selos.controller;
 
+import com.clevel.selos.businesscontrol.ExSummaryControl;
 import com.clevel.selos.businesscontrol.TCGInfoControl;
 import com.clevel.selos.dao.master.PotentialCollateralDAO;
 import com.clevel.selos.dao.master.TCGCollateralTypeDAO;
@@ -72,6 +73,8 @@ public class TCGInfo extends BaseController {
 
     @Inject
     TCGInfoControl tcgInfoControl;
+    @Inject
+    private ExSummaryControl exSummaryControl;
 
     public TCGInfo() {
     }
@@ -206,6 +209,7 @@ public class TCGInfo extends BaseController {
         log.info("onSaveTcgInfo ::: modeForDB  {}", modeForDB);
         try {
             tcgInfoControl.saveTCGInfo(TCGView, TCGDetailViewList, workCaseId);
+            exSummaryControl.calForTCG(workCaseId);
 
             messageHeader = msg.get("app.messageHeader.info");
             message = msg.get("app.tcg.response.save.success");
