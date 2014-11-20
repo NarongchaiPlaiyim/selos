a5a44e9daa831a444a5f3c834c583bc4cf0ba514
Apiruk <apriuk@the-c-level.com>
2014-11-20 18:57:13 +0700

    Add Stateless

--- src/main/java/com/clevel/selos/system/audit/SLOSAuditor.java
+++ src/main/java/com/clevel/selos/system/audit/SLOSAuditor.java
@@ -5,11 +5,12 @@ import com.clevel.selos.model.ActionAudit;
 import com.clevel.selos.model.ActionResult;
 import com.clevel.selos.model.db.audit.SLOSActivity;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 import java.io.Serializable;
 import java.util.Date;
 
-//@Stateless
+@Stateless
 public class SLOSAuditor implements Serializable{
     @Inject
     SLOSActivityDAO slosActivityDAO;
@@ -21,4 +22,5 @@ public class SLOSAuditor implements Serializable{
     public void add(int screenId, String userId, ActionAudit actionAudit, String actionDesc, Date actionDate, ActionResult actionResult, String resultDesc) {
         slosActivityDAO.persist(new SLOSActivity(screenId, userId, actionAudit, actionDesc, actionDate, actionResult, resultDesc, new Date()));
     }
+
 }
