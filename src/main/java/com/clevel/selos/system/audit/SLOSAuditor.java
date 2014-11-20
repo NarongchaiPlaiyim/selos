b20b1cd946194c63fc3fe64296c0456adce99ec7
Apiruk <apriuk@the-c-level.com>
2014-11-20 20:02:08 +0700

    Update

--- src/main/java/com/clevel/selos/system/audit/SLOSAuditor.java
+++ src/main/java/com/clevel/selos/system/audit/SLOSAuditor.java
@@ -1,9 +1,11 @@
 package com.clevel.selos.system.audit;
 
 import com.clevel.selos.dao.audit.SLOSActivityDAO;
+import com.clevel.selos.integration.SELOS;
 import com.clevel.selos.model.ActionAudit;
 import com.clevel.selos.model.ActionResult;
 import com.clevel.selos.model.db.audit.SLOSActivity;
+import org.slf4j.Logger;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -12,6 +14,9 @@ import java.util.Date;
 
 @Stateless
 public class SLOSAuditor implements Serializable{
+    @Inject
+    @SELOS
+    Logger log;
     @Inject
     SLOSActivityDAO slosActivityDAO;
 
@@ -22,5 +27,4 @@ public class SLOSAuditor implements Serializable{
     public void add(int screenId, String userId, ActionAudit actionAudit, String actionDesc, Date actionDate, ActionResult actionResult, String resultDesc) {
         slosActivityDAO.persist(new SLOSActivity(screenId, userId, actionAudit, actionDesc, actionDate, actionResult, resultDesc, new Date()));
     }
-
 }
